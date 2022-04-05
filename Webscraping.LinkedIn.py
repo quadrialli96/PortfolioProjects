a04f35ef2e387ee246db79e160eafbd37907e777
Quadri Alli <96550117+quadrialli96@users.noreply.github.com>
2022-04-05 09:04:46 -0400

    Update Webscraping.LinkedIn.py

--- Webscraping.LinkedIn.py
+++ Webscraping.LinkedIn.py
@@ -6,5 +6,5 @@ linkedin_user = input('Input LinkedIn User: ')
 url = 'https://linkedin.com/' + linkedin_user
 r = requests.get(url)
 soup = bs(r.content, 'html.parser')
-profile_image = soup.find('img', {'alt': 'Quadri Alli'})['src']
+profile_image = soup.find('img', {'alt': 'profile image'})['src']
 print(profile_image)
